9b0178fc5c3252b8044d472c2e10f6b5471ee1ba
Seven <seven@megalive.com.tw>
2017-05-23 21:20:46 +0800

    Modified routes

--- routes/index.js
+++ routes/index.js
@@ -1,84 +1,89 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
 
 module.exports = app => {
-  app.route('/todos')
-    .get((req, res, next) => {
-      req.todoModel
-        .find({})
-        .sort({ 'created_at': 1 })
-        .exec((err, stories) => {
+  app.use('/todos', router);
+
+  router.get('/', (req, res, next) => {
+    req.todoModel
+      .find({})
+      .sort({ 'created_at': 1 })
+      .exec((err, stories) => {
+        if (err) return next();
+
+        res.json(stories);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    const { content = {} } = req.body || {};
+
+    if (content && content.length > 0) {
+      const newTodo = new req.todoModel({
+        content,
+        checked: false,
+        created_at: Date.now(),
+      });
+
+      newTodo.save((err, savedTodo) => {
+        if (err) return next();
+
+        res.json(savedTodo);
+      });
+    } else {
+      return next();
+    }
+  });
+
+  router.delete('/', (req, res) => {
+    req.todoModel.remove(
+      { checked: true },
+      (err) => {
+        if (err) return next();
+
+        res.send(200);
+      },
+    );
+  });
+
+  router.put('/:id', (req, res) => {
+    const { id } = req.params || {};
+    const { checked, content } = req.body || {};
+
+    if (id) {
+      req.todoModel.findOneAndUpdate(
+        { _id: id },
+        Object.assign({},
+          checked !== undefined && { checked },
+          content !== undefined && { content },
+        ),
+        { new: true },
+        (err, newTodo) => {
           if (err) return next();
 
-          res.json(stories);
-        });
-    })
-    .post((req, res) => {
-      const { content = {} } = req.body || {};
-
-      if (content && content.length > 0) {
-        const newTodo = new req.todoModel({
-          content,
-          checked: false,
-          created_at: Date.now(),
-        });
-
-        newTodo.save((err, savedTodo) => {
-          if (err) return next();
-
-          res.json(savedTodo);
-        });
-      } else {
-        return next();
-      }
-    })
-    .delete((req, res) => {
-      req.todoModel.remove(
-        { checked: true },
-        (err) => {
-          if (err) return next();
-
-          res.send(200);
+          res.json(newTodo);
         },
       );
-    });
+    } else {
+      return next();
+    }
+  });
 
-  app.route('/todos/:id')
-    .put((req, res) => {
-      const { id } = req.params || {};
-      const { checked, content } = req.body || {};
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params || {};
 
-      if (id) {
-        req.todoModel.findOneAndUpdate(
-          { _id: id },
-          Object.assign({},
-            checked !== undefined && { checked },
-            content !== undefined && { content },
-          ),
-          { new: true },
-          (err, newTodo) => {
-            if (err) return next();
+    if (id) {
+      req.todoModel.findByIdAndRemove(
+        id,
+        (err, removedTodo) => {
+          if (err) return next();
 
-            res.json(newTodo);
-          },
-        );
-      } else {
-        return next();
-      }
-    })
-    .delete((req, res) => {
-      const { id } = req.params || {};
-
-      if (id) {
-        req.todoModel.findByIdAndRemove(
-          id,
-          (err, removedTodo) => {
-            if (err) return next();
-
-            res.json(removedTodo);
-          },
-        );
-      } else {
-        return next();
-      }
-    });
+          res.json(removedTodo);
+        },
+      );
+    } else {
+      return next();
+    }
+  });
 };
