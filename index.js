e12437ec36ed965ceacec3c414988f114b1bf583
Seven <addhome2001@yahoo.com.tw>
2018-11-30 22:37:49 +0800

    fix typo

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const app = require('express')();
 const webtask = require('webtask-tools');
 const bodyParser = require('body-parser');
-const middlewares = require('./middlewares');
+const middleware = require('./middleware');
 const todosRoutes = require('./todos/routes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(middlewares.db);
+app.use(middleware.db);
 app.use('/todos', todosRoutes);
-app.use(middlewares.errorHandler);
+app.use(middleware.errorHandler);
 
 module.exports = webtask.fromExpress(app);
